cff3a91ad0cd2a6555c4b511546d089133a278bf
perplx <perplx@users.noreply.github.com>
2025-03-23 13:39:18 -0400

    More generic setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 # paths
-venv_name=".venv"
+venv_name='.venv'
 venv_path=$PWD/$venv_name
-activate_path=$venv_path/Scripts/activate
-alias python='py.exe'
+
+# unix paths
+# activate_path="$venv_path/bin/activate"
+
+# windows paths
+activate_path="$venv_path/Scripts/activate"
+alias python='py.exe'  # Windows
 
 # abort if venv already exists
-if [ -e $venv_path ]; then
+if [ -d $venv_path ]; then
     >&2 echo "$venv_path already exists! aborting..."
     exit -1
 fi
@@ -17,5 +22,5 @@ python -m venv $venv_path
 source $activate_path
 pip install --upgrade pip setuptools
 
-# install just-utils in the venv
-pip install .[docs,extra]
+# install project in the venv
+pip install .[extra]
